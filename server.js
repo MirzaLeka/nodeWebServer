078f882fdc35053bd9afc83f38e0443a4fe63366
SPY4ER <mirza_376@hotmail.com>
2018-03-20 21:33:29 +0100

    Added handlebars, partials, helpers and more

--- server.js
+++ server.js
@@ -1,13 +1,43 @@
 const express = require('express');
+const hbs = require("hbs");
+
 var app = express();
+
+hbs.registerHelper("getCurrentYear", () => {
+return new Date().getFullYear();
+});
+
+hbs.registerHelper("screamIt", text => {
+return text.toUpperCase();
+});
+
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view engine", "hbs");
+
+
 
 app.use(express.static(__dirname + '/public', {
     extensions: ['html', 'htm'],
-    // Other options here
+  
 }));
 
-//app.use(express.static(__dirname + '/public')); //middleware
+app.use(express.static(__dirname, {
+    extensions: ['html', 'htm'],
+ 
+}));
 
+
+
+app.listen(3000, () => {
+console.log("Starting Server");
+console.log("Server is running on port 3000");
+});
+
+
+
+//app.use(express.static(__dirname + '/public')); //middleware
+/*
 app.get("/", (request, response) => {
 response.send('<h2 style="color: red">Home Page</h2><br>'
 + '<a href="reservations">Make a reservations</a>');
@@ -16,13 +46,50 @@ name: "Mirza",
 likes : [
 "Biking", 
 "Cities" ]
-});*/
+});
+
+}); */
+
+app.get("/", (req, res) => {
+
+res.render("home.hbs", {
+welcomeMessage: 'Welcome stranger!',
+pageTitle: 'Home Page',
 
 });
+});
+
+
 
 app.get("/about", (req, res) => {
-res.send("<h2>About Page</h2>");
+//res.send("<h2>About Page</h2>");
+res.render("about.hbs", {
+pageTitle: 'About Page',
+
 });
+});
+
+app.get("/reservations", (req, res) => {
+
+res.render("reservations.hbs", {
+pageTitle: 'Make a reservation',
+
+});
+});
+
+
+app.get("/help", (req, res) => {
+
+res.render("help.hbs", {
+pageTitle: 'TRY',
+
+});
+});
+
+
+
+
+/*
 
 app.get("/bad", (req, res) => {
 res.send({
@@ -30,9 +97,8 @@ errorMessage: 'Unable to handle request'
 });
 
 
-});
+})
 
-app.listen(3000, () => {
-console.log("Starting Server");
-console.log("Server is running on port 3000");
-});
+*/
+
+
