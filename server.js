fca23f69c865eada03136a02afa1295a77f7bd22
SPY4ER <mirza_376@hotmail.com>
2018-03-21 23:58:03 +0100

    Site under maintenance => playing with Express middlewares

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const hbs = require("hbs");
+const fs = require("fs");
 
 var app = express();
+
+app.use((req, res, next) => {
+res.render('maintenance.hbs'); // express prati redoslijed operacija. ovdje smo rekli da se renderuje maintenance.hbs te ce to prekriti sve ostale fajlove jer je prije njih (odozgo)
+//tj sve sto je ispod nece se nikad aktivirati
+} );
+
 
 hbs.registerHelper("getCurrentYear", () => {
 return new Date().getFullYear();
@@ -11,18 +18,32 @@ hbs.registerHelper("screamIt", text => {
 return text.toUpperCase();
 });
 
-
 hbs.registerPartials(__dirname + "/views/partials");
+
 app.set("view engine", "hbs");
 
 
+/*
+app.use((req, res, next) => {
+var now = new Date().toString();
+var log = `${now}: ${req.method} ${req.url}`; // prikazuje sta sve GET-a, koji hbs, koji css
 
-app.use(express.static(__dirname + '/public', {
-    extensions: ['html', 'htm'],
+fs.appendFile('server.log', log + '\n', (err) => {
+if (err) {
+console.log("Unable to save file");  }
+});
+
+
+next();
+});
+ */
+
+app.use(express.static(__dirname + '/public', { //for all html files in public directory
+    extensions: ['html', 'htm'], // hide extensions html, htm from link
   
 }));
 
-app.use(express.static(__dirname, {
+app.use(express.static(__dirname, { //for html files in nodeWebServer directory
     extensions: ['html', 'htm'],
  
 }));
